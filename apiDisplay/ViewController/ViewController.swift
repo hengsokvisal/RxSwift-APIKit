7c358254462fb71c925d471f8b94cb892be65c11
hengsokvisal <sokvisal.heng@quad.co.jp>
2018-05-02 16:26:30 +0700

    using PublishSubject

--- apiDisplay/ViewController/ViewController.swift
+++ apiDisplay/ViewController/ViewController.swift
@@ -7,26 +7,32 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
     let lbl = UILabel()
+    let publicSub = PublishSubject<apiResult>()
     
         override func viewDidLoad() {
         super.viewDidLoad()
-            
+       
         view.backgroundColor = UIColor.white
         lbl.frame = CGRect(x:0,y:0,width: 420,height: 1000)
         lbl.textColor = UIColor.black
         lbl.textAlignment = .center
         lbl.numberOfLines = 43
         lbl.lineBreakMode = NSLineBreakMode.byWordWrapping
-        
-        apiService.shared.apiPostRequest { (response) in
-            var api : apiResult
-            api = response
-            self.lbl.text = String(describing: api)
-            }
         self.view.addSubview(self.lbl)
+            
+        apiService.shared.apiPostRequest { (response) in
+            self.publicSub.map{ response in String(describing: response) }
+                .bind(to: self.lbl.rx.text)
+            self.publicSub.subscribe()
+            self.publicSub.onNext(response)
+            }
+    
+        
     }
 }
 
